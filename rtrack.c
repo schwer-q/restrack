941c214b88a1dd2a8e7762d1749862748b710c6d
Antoine Képéklian <antoine.kepeklian@gmail.com>
2015-04-08 16:44:31 -0400

    add call detections

--- rtrack.c
+++ rtrack.c
@@ -25,6 +25,9 @@
  *
  */
 
+
+// TODO :  search fopen open malloc etc ...
+
 #include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -37,119 +40,132 @@
 #include "rtrack.h"
 #include "xalloc.h"
 
-static enum CXChildVisitResult	visitor(CXCursor cursor,
-					CXCursor parent,
-					CXClientData data);
-static char	*visitor_getfilename(CXCursor cursor);
+static enum CXChildVisitResult    visitor(CXCursor cursor,
+                    CXCursor parent,
+                    CXClientData data);
+static char    *visitor_getfilename(CXCursor cursor);
 
 rtrack_t *
 rtrack_new(const char *filename)
 {
-	rtrack_t *this;
+    rtrack_t *this;
 
-	this = xcalloc(1, sizeof(rtrack_t));
-	this->filename = realpath(filename, NULL);
-	this->index = clang_createIndex(1, 1);
-	this->tu = clang_createTranslationUnitFromSourceFile(this->index,
-							     this->filename,
-							     0, NULL, 0, 0);
+    this = xcalloc(1, sizeof(rtrack_t));
+    this->filename = realpath(filename, NULL);
+    this->index = clang_createIndex(1, 1);
+    this->tu = clang_createTranslationUnitFromSourceFile(this->index,
+                                 this->filename,
+                                 0, NULL, 0, 0);
 
-	return (this);
+    return (this);
 }
 
 void
 rtrack_free(rtrack_t *this)
 {
-	clang_disposeTranslationUnit(this->tu);
-	clang_disposeIndex(this->index);
-	free(this->filename);
+    clang_disposeTranslationUnit(this->tu);
+    clang_disposeIndex(this->index);
+    free(this->filename);
 }
 
 void
 rtrack_analyse(rtrack_t *this)
 {
-	CXCursor cursor;
+    CXCursor cursor;
 
-	printf("analyzing %s...\n", this->filename);
-	cursor = clang_getTranslationUnitCursor(this->tu);
-	clang_visitChildren(cursor, visitor, (CXClientData)this);
-	scope_unregister(this, cursor);
+    printf("analyzing %s...\n", this->filename);
+    cursor = clang_getTranslationUnitCursor(this->tu);
+    clang_visitChildren(cursor, visitor, (CXClientData)this);
+    scope_unregister(this, cursor);
 }
 
 static enum CXChildVisitResult
 visitor(CXCursor cursor, CXCursor parent, CXClientData data)
 {
-	rtrack_t *this = (rtrack_t*)data;
-	char *filename;
+    rtrack_t *this = (rtrack_t*)data;
+    char *filename;
 
-	/* (void)parent; */
-	if ((filename = visitor_getfilename(cursor))) {
-		if (strcmp(filename, this->filename))
-			return (CXChildVisit_Continue);
-		printf("%s\n", filename);
-	}
+    /* (void)parent; */
+    if ((filename = visitor_getfilename(cursor))) {
+        if (strcmp(filename, this->filename))
+            return (CXChildVisit_Continue);
+        printf("%s\n", filename);
+    }
 
-	if (scope_unscoped(this, parent)) {
-		printf("=> scope has unscoped\n");
-		scope_unregister(this, parent);
-	}
+    if (scope_unscoped(this, parent)) {
+        printf("=> scope has unscoped\n");
+        scope_unregister(this, parent);
+    }
 
-	switch (clang_getCursorKind(cursor)) {
-	case CXCursor_CompoundStmt: {
-		scope_register(this, parent);
-		printf("=> scope has changed: new scope \033[01;31m#%d\033[00m\n", this->scopelvl);
-		break;
-	}
+    switch (clang_getCursorKind(cursor)) {
+    case CXCursor_CompoundStmt: {
+        scope_register(this, parent);
+        printf("=> scope has changed: new scope \033[01;31m#%d\033[00m\n", this->scopelvl);
+        break;
+    }
 
-	case CXCursor_FunctionDecl: {
-		CXString name = clang_getCursorSpelling(cursor);
-		printf("=> found function declaration: %s\n", clang_getCString(name));
-		clang_disposeString(name);
+    case CXCursor_FunctionDecl: {
+        CXString name = clang_getCursorSpelling(cursor);
+        printf("=> found function declaration: %s\n", clang_getCString(name));
+        clang_disposeString(name);
 
-		scope_register(this, parent);
-		printf("=> scope has changed: new scope \033[01;31m#%d\033[00m\n", this->scopelvl);
-		break;
-	}
+        scope_register(this, parent);
+        printf("=> scope has changed: new scope \033[01;31m#%d\033[00m\n", this->scopelvl);
+        break;
+    }
 
-	case CXCursor_ParmDecl:
-	case CXCursor_VarDecl:
-	{
-		CXString name = clang_getCursorSpelling(cursor);
-		CXType type = clang_getCursorType(cursor);
-		CXString typename = clang_getTypeSpelling(type);
+    case CXCursor_CallExpr:
+    {
+        CXString name = clang_getCursorSpelling(cursor);
+        CXType type = clang_getCursorType(cursor);
+        CXString typename = clang_getTypeSpelling(type);
 
-		printf("=> found variable declaration: \033[01;36m%s\033[00m \033[32m'%s'\033[00m\n",
-		       clang_getCString(name), clang_getCString(typename));
-		clang_disposeString(name);
-		clang_disposeString(typename);
-		variable_register(this, cursor);
-		break;
-	}
+        printf("=> found call declaration: \033[01;36m%s\033[00m \033[32m'%s'\033[00m\n",
+               clang_getCString(name), clang_getCString(typename));
+        clang_disposeString(name);
+        clang_disposeString(typename);
+        break;
+    }
 
-	default:
-		break;
-	}
+    case CXCursor_ParmDecl:
+    case CXCursor_VarDecl:
+    {
+        CXString name = clang_getCursorSpelling(cursor);
+        CXType type = clang_getCursorType(cursor);
+        CXString typename = clang_getTypeSpelling(type);
 
-	return (CXChildVisit_Recurse);
+        printf("=> found variable declaration: \033[01;36m%s\033[00m \033[32m'%s'\033[00m\n",
+               clang_getCString(name), clang_getCString(typename));
+        clang_disposeString(name);
+        clang_disposeString(typename);
+        variable_register(this, cursor);
+        break;
+    }
+
+    default:
+        break;
+    }
+
+    return (CXChildVisit_Recurse);
 }
 
 static char *
 visitor_getfilename(CXCursor cursor)
 {
-	CXFile _file;
-	CXSourceLocation _location;
-	CXString _filename;
-	static char filename[PATH_MAX];
+    CXFile _file;
+    CXSourceLocation _location;
+    CXString _filename;
+    static char filename[PATH_MAX];
 
-	bzero(filename, sizeof(filename));
+    bzero(filename, sizeof(filename));
 
-	_location = clang_getCursorLocation(cursor);
-	clang_getFileLocation(_location, &_file, 0, 0, 0);
-	if (!_file)
-		return (NULL);
-	_filename = clang_getFileName(_file);
+    _location = clang_getCursorLocation(cursor);
+    clang_getFileLocation(_location, &_file, 0, 0, 0);
+    if (!_file)
+        return (NULL);
+    _filename = clang_getFileName(_file);
 
-	(void)strlcpy(filename, clang_getCString(_filename), PATH_MAX);
-	clang_disposeString(_filename);
-	return (filename);
+    (void)strlcpy(filename, clang_getCString(_filename), PATH_MAX);
+    clang_disposeString(_filename);
+    return (filename);
 }
