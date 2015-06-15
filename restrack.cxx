7ce410dc4847640c8a44edc38d68a07e92f0cd7a
Quentin Schwerkolt <schwer_q@outlook.com>
2015-06-15 14:24:54 +0200

    Fix spelling
    --HG--
    branch : clang-plugin

--- restrack.cxx
+++ restrack.cxx
@@ -29,7 +29,7 @@
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Frontend/CompilerInstance.h"
-#include "clang/Frontend/FrontentAction.h"
+#include "clang/Frontend/FrontendAction.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
 #include "llvm/Support/raw_ostream.h"
 
