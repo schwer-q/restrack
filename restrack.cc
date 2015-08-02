4f9b0678ca126c0157d265078691a91fa6bed020
Quentin Schwerkolt <schwer_q@outlook.com>
2015-08-02 11:25:17 +0200

    Try clang::ParentMap #2
    --HG--
    branch : clang-plugin

--- restrack.cc
+++ restrack.cc
@@ -123,7 +123,7 @@ RessourceTrackerVisitor::VisitBinaryOperator(clang::BinaryOperator *Operator)
 bool
 RessourceTrackerVisitor::VisitCompoundStmt(clang::CompoundStmt *Expr)
 {
-	clang::ParentMap PM = clang::ParentMap(this->Context->getTranslationUnitDecl());
+	clang::ParentMap PM(this->Context->getTranslationUnitDecl());
 	return (true);
 }
 
