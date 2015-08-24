7cc2d5a6c15820a88bd94aa78112ec42a4ac8354
Quentin Schwerkolt <schwer_q@outlook.com>
2015-08-24 17:19:29 +0200

    drop old code
    --HG--
    branch : clang-plugin

--- restrack.cc
+++ restrack.cc
@@ -39,57 +39,6 @@
 
 #include "restrack.hh"
 
-#if 0
-namespace {
-
-	class PrintFunctionsConsumer : public ASTConsumer {
-	public:
-		virtual bool HandleTopLevelDecl(DeclGroupRef DG) {
-			for (DeclGroupRef::iterator it = DG.begin(), e = DG.end(); it != e; ++it) {
-				const Decl *D = *it;
-				if (const NamedDecl *ND = dyn_cast<NamedDecl>(D)) {
-					std::string name = ND->getNameAsString();
-					if (name == "malloc")
-						llvm::errs() << "top-level-decl: \"" << name << "\"\n";
-				}
-			}
-			return (true);
-		}
-	};
-
-	class PrintFunctionNamesAction : public PluginASTAction {
-	protected:
-		std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
-							       llvm::StringRef) {
-			return (llvm::make_unique<PrintFunctionsConsumer>());
-		}
-
-		bool ParseArgs(const CompilerInstance &CI,
-			       const std::vector<std::string>& args) {
-			for (unsigned i = 0, e = args.size(); i != e; ++i) {
-				llvm::errs() << "PrintFunctionNames arg = " << args[i] << "\n";
-
-				if (args[i] == "-an-error") {
-					DiagnosticsEngine &D = CI.getDiagnostics();
-					unsigned DiagID = D.getCustomDiagID(DiagnosticsEngine::Error,
-									    "invalid argument '%0'");
-					D.Report(DiagID) << args[i];
-					return (false);
-				}
-			}
-			if (args.size() && args[0] == "help")
-				PrintHelp(llvm::errs());
-
-			return (true);
-		}
-
-		void PrintHelp(llvm::raw_ostream& ros) {
-			ros << "Help for PrintFunctionNames plugin goes here\n";
-		}
-	};
-}
-#endif	// 0
-
 RessourceTrackerFunction::RessourceTrackerFunction(clang::FunctionDecl *Function)
 {
 	this->m_Function = Function;
